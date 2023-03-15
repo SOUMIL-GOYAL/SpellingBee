9639aa899acdb5e5cb15ed4fd26a9122aa5b3e80
MasterDhruvGoyal <dhruv.goyal0111@gmail.com>
2023-03-15 17:42:23 -0500

    can read from file words in index

--- Website/word-def-speech.js
+++ Website/word-def-speech.js
@@ -1,8 +1,8 @@
-let generatedWord = '';
+/*let generatedWord = '';
 
 function generateWord() {
   // Array of random words
-  const words = document.getElementById(["hard"]).textContent;
+  const words = document.getElementById("hard").textContent.split(", ");
   // Generate a random word from the array
   generatedWord = words[Math.floor(Math.random() * words.length)];
   // Call the text-to-speech function with the generated word as input
@@ -26,3 +26,65 @@ function speak(text) {
   const utterance = new SpeechSynthesisUtterance(text);
   synth.speak(utterance);
 }
+*/
+
+let generatedWord = '';
+
+function generateWord() {
+  // Get the text content of the <p> tag
+  const words = document.getElementById("hard").textContent.trim().split(" , ");
+  // Generate a random word from the array
+  generatedWord = words[Math.floor(Math.random() * words.length)];
+  // Call the text-to-speech function with the generated word as input
+  speak(`The word is ${generatedWord}`);
+}
+
+function getDefinition() {
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${generatedWord}`)
+    .then(response => response.json())
+    .then(data => {
+      const definition = data[0].meanings[0].definitions[0].definition;
+      // Call the text-to-speech function with the extracted definition as input
+      speak(`The definition of ${generatedWord} is: ${definition}`);
+    })
+    .catch(error => console.log(error));
+}
+
+// Function to convert text to speech using the Web Speech API
+function speak(text) {
+  const synth = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance(text);
+  synth.speak(utterance);
+}
+
+
+//original
+/*let generatedWord = '';
+
+function generateWord() {
+  // Array of random words
+  const words = ['apple', 'banana', 'chocolate', 'diamond', 'elephant', 'football', 'guitar', 'hamburger', 'internet', 'jacket'];
+  // Generate a random word from the array
+  generatedWord = words[Math.floor(Math.random() * words.length)];
+  // Call the text-to-speech function with the generated word as input
+  speak(`The word is ${generatedWord}`);
+}
+
+function getDefinition() {
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${generatedWord}`)
+    .then(response => response.json())
+    .then(data => {
+      const definition = data[0].meanings[0].definitions[0].definition;
+      // Call the text-to-speech function with the extracted definition as input
+      speak(`The definition of ${generatedWord} is: ${definition}`);
+    })
+    .catch(error => console.log(error));
+}
+
+// Function to convert text to speech using the Web Speech API
+function speak(text) {
+  const synth = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance(text);
+  synth.speak(utterance);
+}
+*/
